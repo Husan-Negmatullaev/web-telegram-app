def68f283ba9de6a9067a1e06b31b127e18472ae
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2022-10-24 19:13:31 +0500

    changes

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import BottomNavBar from "../../components/BottomNavBar";
-import Header from "../../components/Header";
+// import Header from "../../components/Header";
 
 const MainLayout = () => {
   return (
