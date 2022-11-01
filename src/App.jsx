61950d5e74966fa0dba690baa68ad3cfc879fb2d
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2022-11-01 16:53:32 +0500

    release-version

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
   React.useEffect(() => {
     tg.ready();
     // console.log();
-    dispatch(fetchFavorites("503118393"));
-    // dispatch(fetchFavorites(tg.initDataUnsafe?.user?.id));
+    // dispatch(fetchFavorites("503118393"));
+    dispatch(fetchFavorites(tg.initDataUnsafe?.user?.id));
   }, [])
 
   return (
