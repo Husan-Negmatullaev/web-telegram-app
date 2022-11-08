2ad757a16febe821b434e44716ebcc9546cda35d
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2022-11-08 19:25:33 +0500

    change-formatting-number

--- src/components/ChannelInfo/index.jsx
+++ src/components/ChannelInfo/index.jsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
-import { abbreviateNumber } from "../../helpers/functions";
 import { ButtonLink } from "../common/Button";
 
 import styles from './ChannelInfo.module.scss';
 
 const ChannelInfo = ({ tgstatLink, telemetrLink, cpm, err, coverage }) => {
+  const formatter = Intl.NumberFormat("en", {
+    notation: "compact"
+  });
 
   return (
     <div className={styles.channelContent__body}>
@@ -19,15 +21,15 @@ const ChannelInfo = ({ tgstatLink, telemetrLink, cpm, err, coverage }) => {
       <div className={styles.channelContent__content}>
         <div className={styles.channelContent__data}>
           <span className={styles.channelContent__placeholder}>Охваты</span>
-          <p className={styles.channelContent__text}>{abbreviateNumber(coverage)}</p>
+          <p className={styles.channelContent__text}>{formatter.format(coverage)}</p>
         </div>
         <div className={styles.channelContent__data}>
           <span className={styles.channelContent__placeholder}>ERR</span>
-          <p className={styles.channelContent__text}>{abbreviateNumber(err)}</p>
+          <p className={styles.channelContent__text}>{formatter.format(err)}</p>
         </div>
         <div className={styles.channelContent__data}>
           <span className={styles.channelContent__placeholder}>CPM</span>
-          <p className={styles.channelContent__text}>{abbreviateNumber(cpm)}</p>
+          <p className={styles.channelContent__text}>{formatter.format(cpm)}</p>
         </div>
       </div>
     </div>
